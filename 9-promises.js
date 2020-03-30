47a4eb84916429207dacd8a6ed682bea3fc203fe
BipinKalra <bipinkalra@gmail.com>
2020-03-30 20:57:14 +0530

    promise chaining

--- 9-promises.js
+++ 9-promises.js
@@ -1,8 +1,8 @@
 // This is essentially a promise
 const doWorkPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve([1,2,3,4])
-    reject("This is my error!")
+    resolve([1,2,3,4])
+    // reject("This is my error!")
   }, 2000)
 })
 
@@ -12,5 +12,42 @@ doWorkPromise.then((result) => {
 }).catch((error) => {
   // .catch runs when reject is called from the promise i.e. failure case
 
+  console.log(error)
+})
+
+// Example for promise chaining
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 2000)
+  })
+}
+
+// Promise chaining with basic nesting syntax
+
+// add(4,5).then((sum) => {
+//   console.log(sum)
+
+//   // Nested call to the promise in order to execute 2 asynchronous tasks in a chain
+//   add(sum, 10).then((sum2) => {
+//     console.log(sum2)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+// Promise chaining with better clean syntax
+// This results in code that is not nested and thus looks sleek
+// Furthermore, only a single catch call is required
+add(4,5).then((sum) => {
+  console.log(sum)
+
+  return add(sum, 10)
+}).then((sum2) => {
+  console.log(sum2)
+}).catch((error) => {
   console.log(error)
 })
